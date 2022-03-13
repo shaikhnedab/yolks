ae4486b9b55a8df3df555285c61d79de75a9f6c5
Shaikh Nedab <shaikhnedab@gmail.com>
2022-03-13 19:53:02 +0530

    first upload

---
+++ nodejs/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cd /home/container
+
+# Make internal Docker IP address available to processes.
+export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
+
+# Print Node.js Version
+node -v
+# Print NPM Version
+npm -v
+# Install NPM Packages
+npm i
+
+# Replace Startup Variables
+MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+eval ${MODIFIED_STARTUP}
