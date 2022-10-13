424a6e7f67f2496d4ffbb98ea52aff071c5d8857
Shaikh Nedab <55206494+shaikhnedab@users.noreply.github.com>
2022-10-14 02:20:42 +0530

    upl

--- gsmtest/entrypoint.sh
+++ gsmtest/entrypoint.sh
@@ -4,6 +4,19 @@ cd /home/container
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
+# Print Node.js Version
+node -v
+# Print NPM Version
+npm -v
+# Print Python Version
+python3.9 --version
+# Print PIP Version
+python3.9 -m pip --version
+# Install Python Requirements
+python3.9 -m pip install -r requirements.txt
+# Build NPM Packages
+npm run build
+
 # Replace Startup Variables
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo ":/home/container$ ${MODIFIED_STARTUP}"
