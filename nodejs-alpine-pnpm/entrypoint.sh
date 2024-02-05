7b73635065e301fe6ece5369cc03d79138cefdc4
shaikhnedab <shaikhnedab@gmail.com>
2024-02-05 15:36:47 +0530

    new yolk

---
+++ nodejs-alpine-pnpm/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/ash
+cd /home/container
+
+# Make internal Docker IP address available to processes.
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
+
+echo "Node.js Version"
+node -v
+echo "NPM Version"
+npm -v
+echo "NPM Install Packages"
+npm i
+
+# Replace Startup Variables
+MODIFIED_STARTUP=$(echo -e $(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
+echo -e ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+eval ${MODIFIED_STARTUP}
